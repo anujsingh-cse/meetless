6ea5ee6c75278ecc30e4400790e7ad8049b15c73
anujsingh-cse <anujsingh0543@gmail.com>
2026-08-25 22:28:38 +0530

    fix: add root ESLint and Prettier configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['dist/', 'node_modules/', '*.config.*']
+}
