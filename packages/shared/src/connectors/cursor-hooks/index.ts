326c4372c4fc1e5e072e3c0b0887c826b4794deb
anujsingh-cse <anujsingh0543@gmail.com>
2026-08-27 13:35:46 +0530

    feat(cursor-hooks): add barrel exports for CursorHooksConnector

---
+++ packages/shared/src/connectors/cursor-hooks/index.ts
@@ -0,0 +1,11 @@
+export { CursorHooksConnector } from './connector.js'
+export type { CursorHooksConnectorOptions } from './connector.js'
+export { BridgeServer } from './bridge.js'
+export { HookInstaller } from './hook-installer.js'
+export { normalizeCursorHookEvent } from './normalizer.js'
+export {
+  CURSOR_HOOK_EVENT_NAMES,
+  isCursorHookEventName,
+  BRIDGE_SECRET_HEADER,
+} from './types.js'
+export type { CursorHookEvent, CursorHookEventName, CursorHookPayload } from './types.js'
